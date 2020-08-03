257e358fcc587f71081384e2d7d153ef05ad56bf
hamzailyas237 <hamzailyas237@gmail.com>
2020-08-03 11:12:47 +0500

    my todo app

---
+++ app.js
@@ -0,0 +1,39 @@
+var list = document.getElementById("list");
+
+function addTodo(){
+    var todoItem = document.getElementById("todo_item");
+    var li = document.createElement("li");
+    var liText = document.createTextNode(todo_item.value);
+    li.appendChild(liText);
+    list.appendChild(li);
+    todo_item.value = "";
+    var hr = document.createElement("hr")
+    li.appendChild(hr)
+    console.log(li)
+
+    var delBtn = document.createElement("button");
+    var delText = document.createTextNode("DELETE");
+    delBtn.appendChild(delText);
+    li.appendChild(delBtn)
+    delBtn.setAttribute("onclick","deleteItem(this)")
+
+    var editBtn = document.createElement("button");
+    var editText = document.createTextNode("EDIT");
+    editBtn.appendChild(editText);
+    li.appendChild(editBtn);
+    editBtn.setAttribute("onclick","editItem(this)")
+}
+
+function deleteItem(e){
+    e.parentNode.remove()
+}
+
+function deleteAll(){
+    list.innerHTML = ""
+}
+
+function editItem(e){
+   var val = e.parentNode.firstChild.nodeValue;
+   var editVal = prompt("enter new",val);
+   e.parentNode.firstChild.nodeValue = editVal
+}
